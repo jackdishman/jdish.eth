14d76a217517eff2eca2262b812d09dee2a4e777
jack dishman <me@jackdishman.com>
2021-08-03 20:40:00 -0700

    updating dependencies

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+	<main>
+    <Header />
+		<Nuxt />
+	</main>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+	created () {
+	}
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <Header />
+    
   </main>
 </template>
 
